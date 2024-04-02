64a765a80a52cbcc94ad6eb88178af3319d0479d
Alireza <alireza.mohammadi7878@gmail.com>
2024-04-02 23:20:13 +0330

    add diary system & fix dialogue bug when jumping

--- Assets/_Scripts/Controllers/DialogueController.cs
+++ Assets/_Scripts/Controllers/DialogueController.cs
@@ -6,164 +6,170 @@ using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
 {
-    //dialogue box
-    private TextMeshProUGUI NPCName;
-    private TextMeshProUGUI NPCDialogue;
-    private Image boxImage;
-    private readonly float defaultTypeSpeed = 0.02f;
+	//dialogue box
+	[SerializeField]
+	private GameObject dialogueBox;
+	private TextMeshProUGUI NPCName;
+	private TextMeshProUGUI NPCDialogue;
+	private Image boxImage;
+	private readonly float defaultTypeSpeed = 0.02f;
 
-    //paragraphs
-    private readonly Queue<string> names = new();
-    private readonly Queue<string> paragraphs = new();
-    private string currentParagraph;
-    private string currentName;
-    private Coroutine typeDialogueCoroutine;
+	//paragraphs
+	private readonly Queue<string> names = new();
+	private readonly Queue<string> paragraphs = new();
+	private string currentParagraph;
+	private string currentName;
+	private Coroutine typeDialogueCoroutine;
 
-    //punctuations
-    private readonly Dictionary<HashSet<char>, float> punctuations = new()
-    {
-        { new HashSet<char>(){'.', '?', '!'}, 0.3f },
-        { new HashSet<char>(){',', ';', ':'}, 0.1f}
-    };
+	//punctuations
+	private readonly Dictionary<HashSet<char>, float> punctuations = new()
+	{
+		{ new HashSet<char>(){'.', '?', '!'}, 0.3f },
+		{ new HashSet<char>(){',', ';', ':'}, 0.1f}
+	};
 
-    //dependencies
-    private Player playerScript;
+	//controllers
+	private bool endOfconversation;
+	private bool isTyping;
+	private bool isDelayed = false;
 
-    //controllers
-    private bool endOfconversation;
-    private bool isTyping;
-    private bool isDelayed = false;
+	//dependencies
+	private Player playerScript;
+	private Controller2D controller2D;
 
-    private void Awake()
-    {
-        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	private void Awake()
+	{
+		playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+		controller2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Controller2D>();
 
-        var temp = GetComponentsInChildren<TextMeshProUGUI>();
-        NPCName = temp[0];
-        NPCDialogue = temp[1];
+		var temp = dialogueBox.GetComponentsInChildren<TextMeshProUGUI>();
+		NPCName = temp[0];
+		NPCDialogue = temp[1];
 
-        boxImage = gameObject.GetComponent<Image>();
-    }
+		boxImage = dialogueBox.GetComponent<Image>();
+	}
 
-    public void DisplayNextParagraph(DialogueText dialogueText)
-    {
-        //handle empty paragraphs
-        if (paragraphs.Count == 0)
-        {
-            if (!endOfconversation)
-                StartConversation(dialogueText);
+	public void DisplayNextParagraph(DialogueText dialogueText)
+	{
+		//handle empty paragraphs
+		if (paragraphs.Count == 0)
+		{
+			if (!endOfconversation && controller2D.info.bottomCollision)
+				StartConversation(dialogueText);
 
-            else if (endOfconversation && !isTyping)
-            {
-                EndConversation();
-                return;
-            }
-        }
+			else if (endOfconversation && !isTyping)
+			{
+				EndConversation();
+				return;
+			}
+			else
+				return;
+		}
 
-        //handle writting paragrpahs
-        if (!isTyping && !isDelayed)
-        {
-            currentParagraph = paragraphs.Dequeue();
-            currentName = names.Dequeue();
+		//handle writting paragrpahs
+		if (!isTyping && !isDelayed)
+		{
+			currentParagraph = paragraphs.Dequeue();
+			currentName = names.Dequeue();
 
-            //delay if text is empty
-            if (string.IsNullOrWhiteSpace(currentParagraph))
-            {
-                StartCoroutine(Delay(currentName));
-            }
-            else
-            {
-                boxImage.enabled = true;
-                typeDialogueCoroutine = StartCoroutine(TypeDialogue(currentName, currentParagraph));
-            }
-        }
-        else
-        {
-            FinishParagraph(currentParagraph);
-        }
+			//delay if text is empty
+			if (string.IsNullOrWhiteSpace(currentParagraph))
+			{
+				StartCoroutine(Delay(currentName));
+			}
+			else
+			{
+				boxImage.enabled = true;
+				typeDialogueCoroutine = StartCoroutine(TypeDialogue(currentName, currentParagraph));
+			}
+		}
+		else
+		{
+			FinishParagraph(currentParagraph);
+		}
 
-        if (paragraphs.Count == 0)
-            endOfconversation = true;
-    }
+		if (paragraphs.Count == 0)
+			endOfconversation = true;
+	}
 
-    private void StartConversation(DialogueText dialogueText)
-    {
-        gameObject.SetActive(true);
-        playerScript.enabled = false;
+	private void StartConversation(DialogueText dialogueText)
+	{
+		dialogueBox.SetActive(true);
+		playerScript.enabled = false;
 
-        foreach (var n in dialogueText.speakerNames)
-            names.Enqueue(n);
+		foreach (var n in dialogueText.speakerNames)
+			names.Enqueue(n);
 
-        foreach (var p in dialogueText.paragraphs)
-            paragraphs.Enqueue(p);
-    }
+		foreach (var p in dialogueText.paragraphs)
+			paragraphs.Enqueue(p);
+	}
 
-    private void EndConversation()
-    {
-        paragraphs.Clear();
-        names.Clear();
-        endOfconversation = false;
+	private void EndConversation()
+	{
+		paragraphs.Clear();
+		names.Clear();
+		endOfconversation = false;
 
-        playerScript.enabled = true;
-        playerScript.GetMovementInput(Vector2.zero);
+		playerScript.enabled = true;
+		playerScript.GetMovementInput(Vector2.zero);
 
-        gameObject.SetActive(false);
-    }
+		dialogueBox.SetActive(false);
+	}
 
-    private IEnumerator TypeDialogue(string name, string text)
-    {
-        //assigning text and color values 
-        isTyping = true;
-        var visibleCharactersCount = 0;
+	private IEnumerator TypeDialogue(string name, string text)
+	{
+		//assigning text and color values 
+		isTyping = true;
+		var visibleCharactersCount = 0;
 
-        boxImage.material.color = Color.black;
-        var speakerColor = NPCsColors.GetColor(name);
-        NPCDialogue.color = speakerColor;
-        NPCName.color = speakerColor;
+		boxImage.material.color = Color.black;
+		var speakerColor = NPCsColors.GetColor(name);
+		NPCDialogue.color = speakerColor;
+		NPCName.color = speakerColor;
 
-        NPCName.text = name;
-        NPCDialogue.text = text;
-        NPCDialogue.maxVisibleCharacters = visibleCharactersCount;
+		NPCName.text = name;
+		NPCDialogue.text = text;
+		NPCDialogue.maxVisibleCharacters = visibleCharactersCount;
 
-        //displaying text letter by letter
-        var lastCharacter = '.';
+		//displaying text letter by letter
+		var lastCharacter = '.';
 
-        foreach (var c in text)
-        {
-            var pauseDuration = defaultTypeSpeed;
-            visibleCharactersCount++;
-            NPCDialogue.maxVisibleCharacters = visibleCharactersCount;
+		foreach (var c in text)
+		{
+			var pauseDuration = defaultTypeSpeed;
+			visibleCharactersCount++;
+			NPCDialogue.maxVisibleCharacters = visibleCharactersCount;
 
-            //pause after punctuations
-            foreach (KeyValuePair<HashSet<char>, float> category in punctuations)
-            {
-                if (c != lastCharacter && category.Key.Contains(c))
-                    pauseDuration = category.Value;
-            }
+			//pause after punctuations
+			foreach (KeyValuePair<HashSet<char>, float> category in punctuations)
+			{
+				if (c != lastCharacter && category.Key.Contains(c))
+					pauseDuration = category.Value;
+			}
 
-            lastCharacter = c;
-            yield return new WaitForSeconds(pauseDuration);
-        }
+			lastCharacter = c;
+			yield return new WaitForSeconds(pauseDuration);
+		}
 
-        isTyping = false;
-    }
+		isTyping = false;
+	}
 
-    private void FinishParagraph(string p)
-    {
-        StopCoroutine(typeDialogueCoroutine);
-        NPCDialogue.maxVisibleCharacters = p.Length;
-        isTyping = false;
-    }
+	private void FinishParagraph(string p)
+	{
+		StopCoroutine(typeDialogueCoroutine);
+		NPCDialogue.maxVisibleCharacters = p.Length;
+		isTyping = false;
+	}
 
-    private IEnumerator Delay(string delay)
-    {
-        boxImage.enabled = false;
-        NPCName.text = "";
-        NPCDialogue.text = "";
-        isDelayed = true;
+	private IEnumerator Delay(string delay)
+	{
+		boxImage.enabled = false;
+		NPCName.text = "";
+		NPCDialogue.text = "";
+		isDelayed = true;
 
-        yield return new WaitForSeconds(float.Parse(delay));
+		yield return new WaitForSeconds(float.Parse(delay));
 
-        isDelayed = false;
-    }
+		isDelayed = false;
+	}
 }
---
+++ Assets/_Scripts/DiaryController.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class DiaryController : MonoBehaviour
+{
+	//diary elements
+	[SerializeField]
+	private GameObject cover;
+	private Image leftPage;
+	private Image rightPage;
+
+	//left & right pages index
+	private int leftIndex = 0;
+	private int rightIndex = 1;
+
+	//maximum pages control
+	[SerializeField]
+	private int maxIndex = 1;
+
+	[SerializeField]
+	private Sprite[] pages;
+
+	//dependencies
+	private Player playerScript;
+	private Controller2D controller2D;
+
+	private void Awake()
+	{
+		playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+		controller2D = GameObject.FindGameObjectWithTag("Player").GetComponent<Controller2D>();
+
+		var temp = cover.GetComponentsInChildren<Image>();
+		leftPage = temp[1];
+		rightPage = temp[2];
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Keyboard.current.tabKey.wasPressedThisFrame && controller2D.info.bottomCollision)
+		{
+			if (!cover.activeSelf)
+			{
+				cover.SetActive(true);
+				playerScript.enabled = false;
+				DisplayDiary();
+			}
+			else
+			{
+				cover.SetActive(false);
+				playerScript.enabled = true;
+				playerScript.GetMovementInput(Vector2.zero);
+			}
+		}
+
+		if (cover.activeSelf)
+		{
+			if (Keyboard.current.aKey.wasPressedThisFrame)
+				TurnPage(-2);
+			else if (Keyboard.current.dKey.wasPressedThisFrame)
+				TurnPage(2);
+		}
+	}
+
+	private void TurnPage(int v)
+	{
+		leftIndex += v;
+		rightIndex += v;
+
+		//checking first and last page indexes
+		if (leftIndex < 0)
+		{
+			leftIndex = 0;
+			rightIndex = 1;
+			return;
+		}
+		else if (rightIndex > maxIndex)
+		{
+			leftIndex = maxIndex - 1;
+			rightIndex = maxIndex;
+			return;
+		}
+
+		DisplayDiary();
+	}
+
+	private void DisplayDiary()
+	{
+		if (leftIndex >= 0 && rightIndex <= maxIndex)
+		{
+			leftPage.overrideSprite = pages[leftIndex];
+			rightPage.overrideSprite = pages[rightIndex];
+		}
+	}
+
+	//unlocking new entries in diary
+	public void IncreamentDiaryIndex()
+	{
+		maxIndex += 2;
+	}
+}
